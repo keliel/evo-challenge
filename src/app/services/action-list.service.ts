3765e6c778b4e5c28d9c7db30de7da05523ae7d9
Hannah Schinz <hannah.schinz@gmail.com>
2021-04-10 22:59:03 +0200

    Use uuids for hardcoded version of treeview list

--- src/app/services/action-list.service.ts
+++ src/app/services/action-list.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { v1 as uuid } from 'uuid';
 import { ActionPointType } from '../core/models/action-point-type.enum';
 
 @Injectable({
@@ -9,37 +10,43 @@ export class ActionListService {
 
   private actionPointList = [
     {
-      id: 'sdfsfsf',
+      id: uuid(),
       title: 'Visualize impactful mindshare',
       subactions: [{
-        id: 'sdfwfsf',
+        id: uuid(),
         title: 'Monetize B2B portals',
         type: ActionPointType.Goal,
-        subactions: [{
-          id: 'sdfawfsf',
-          title: 'Streamline customized models',
-          type: ActionPointType.Decision,
-          subactions: [{
-            id: 'sdfawfadsf',
-            title: 'Architect ubiquitous deliverables',
-            type: ActionPointType.Task,
-          }]
-        }]
       }]
     },
     {
-      id: 'sdfsf1f',
+      id: uuid(),
+      title: 'Enable value-added convergence',
+      subactions: [{
+        id: uuid(),
+        title: 'Something hidden',
+      }]
+    },
+    {
+      id: uuid(),
+      title: 'Visualize vertical interfaces',
+      subactions: [{
+        id: uuid(),
+        title: 'Something hidden',
+      }]
+    },
+    {
+      id: uuid(),
       title: 'Integrate clicks-and-mortar methodologies',
       subactions: [{
-        id: 'sdfwfsf',
+        id: uuid(),
         title: 'Monetize B2B portals',
         type: ActionPointType.Goal,
         subactions: [{
-          id: 'sdfawfsf',
+          id: uuid(),
           title: 'Streamline customized models',
           type: ActionPointType.Decision,
           subactions: [{
-            id: 'sdfawfadsf',
+            id: uuid(),
             title: 'Architect ubiquitous deliverables',
             type: ActionPointType.Task,
           }]
@@ -47,9 +54,110 @@ export class ActionListService {
       }]
     },
     {
-      id: 'sadfsf1f',
-      title: 'Integrate cslicks-and-mortar methodologies',
-    }
+      id: uuid(),
+      title: 'Enable efficient interfaces',
+      subactions: [{
+        id: uuid(),
+        title: 'Something hidden',
+      }]
+    },
+    {
+      id: uuid(),
+      title: 'Monetize ubiquitous deliverables',
+      subactions: [{
+        id: uuid(),
+        title: 'Something hidden',
+      }]
+    },
+    {
+      id: uuid(),
+      title: 'Repurpose viral metrics',
+      subactions: [{
+        id: uuid(),
+        title: 'Something hidden',
+      }]
+    },
+    {
+      id: uuid(),
+      title: 'Benchmark open-source technologies',
+      subactions: [{
+        id: uuid(),
+        title: 'Something hidden',
+      }]
+    },
+    {
+      id: uuid(),
+      title: 'Envisioneer cross-platform methodologies',
+      subactions: [{
+        id: uuid(),
+        title: 'Something hidden',
+      }]
+    },
+    {
+      id: uuid(),
+      title: 'Deploy B2C mindshare',
+      subactions: [{
+        id: uuid(),
+        title: 'Something hidden',
+      }]
+    },
+    {
+      id: uuid(),
+      title: 'Disintermediate interactive architectures',
+      secured: true,
+      subactions: [{
+        id: uuid(),
+        title: 'Something hidden',
+      }]
+    },
+    {
+      id: uuid(),
+      title: 'Matrix holistic deliverables',
+      subactions: [{
+        id: uuid(),
+        title: 'Something hidden',
+      }]
+    },
+    {
+      id: uuid(),
+      title: 'Incentivize open-source niches',
+      subactions: [{
+        id: uuid(),
+        title: 'Something hidden',
+      }]
+    },
+    {
+      id: uuid(),
+      title: 'Mesh enterprise infomediaries',
+      subactions: [{
+        id: uuid(),
+        title: 'Something hidden',
+      }]
+    },
+    {
+      id: uuid(),
+      title: 'Engage customized paradigms',
+      subactions: [{
+        id: uuid(),
+        title: 'Something hidden',
+      }]
+    },
+    {
+      id: uuid(),
+      title: 'Orchestrate back-end schemas',
+      subactions: [{
+        id: uuid(),
+        title: 'Something hidden',
+      }]
+    },
+    {
+      id: uuid(),
+      title: 'Engage real-time ROI',
+      subactions: [{
+        id: uuid(),
+        title: 'Something hidden',
+      }]
+    },
   ];
 
   constructor() { }
