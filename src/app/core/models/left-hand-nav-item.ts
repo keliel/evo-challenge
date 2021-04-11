80239b0a886ae8603f66e86edd57eda7dc4b48d7
Hannah Schinz <hannah.schinz@gmail.com>
2021-04-11 19:43:26 +0200

    Move interface to component itself, since it is not used anywhere else

--- src/app/core/models/left-hand-nav-item.ts
+++
@@ -1,9 +0,0 @@
-
-import { IconDefinition } from '@fortawesome/free-solid-svg-icons';
-
-export interface LeftHandNavItem {
-  icon: IconDefinition;
-  link: string;
-  muted?: boolean;
-  hasUpdates?: boolean;
-}
