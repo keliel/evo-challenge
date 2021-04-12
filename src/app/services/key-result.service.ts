416f1ca4954be4555073a4ee016b49736a5f74d4
Hannah Schinz <hannah.schinz@gmail.com>
2021-04-12 18:09:13 +0200

    Style kendo grid

--- src/app/services/key-result.service.ts
+++ src/app/services/key-result.service.ts
@@ -15,11 +15,19 @@ export class KeyResultService {
       progress: '50',
       status: 0,
     }, {
-      text: 'We achieved a sales growth in Power Management Applications globally from 65 Mio.€ in 2019 to 128 Mio.€ in 2024',
+      text: 'We refocussed the Automotive sales team globally to increase personal customer touch points Tier1 to 60 p.a.',
       progress: '50',
       status: 0,
     }, {
-      text: 'We reconfigured and applied already existing Technology Portfolio within CTG Group successfully to enlarge our addressable.',
+      text: 'We established the needed application engineering knowledge locally and conducted all product and customer related trainings needed',
+      progress: '50',
+      status: 0,
+    }, {
+      text: 'We increased our customer penetration rate to 30%  p.a. for TOP5-Tier 1 with existing Power Management solutions in average',
+      progress: '50',
+      status: 0,
+    }, {
+      text: 'We increased our customer penetration rate to 30%  p.a. for TOP5-Tier 2 with existing Power Management solutions in average',
       progress: '50',
       status: 0,
     },
